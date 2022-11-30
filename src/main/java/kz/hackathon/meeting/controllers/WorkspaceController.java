3d4df900f4683b91ad44e25df547d16f4cc5d256
BakhytzhanAmanzholov <amanzholovbakhytzhan@gmail.com>
2022-11-30 15:09:18 +0600

    Workspace

--- src/main/java/kz/hackathon/meeting/controllers/WorkspaceController.java
+++ src/main/java/kz/hackathon/meeting/controllers/WorkspaceController.java
@@ -1,6 +1,7 @@
 package kz.hackathon.meeting.controllers;
 
 import kz.hackathon.meeting.dto.mappers.ScheduleWorkspaceMapper;
+import kz.hackathon.meeting.dto.response.ScheduleWorkspaceDto;
 import kz.hackathon.meeting.models.ScheduleWorkspace;
 import kz.hackathon.meeting.services.WorkspaceService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -23,5 +27,16 @@ public class WorkspaceController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return ResponseEntity.ok(ScheduleWorkspaceMapper.toResponseDto(workspace));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getSchedule(@PathVariable("id") Long id){
+        List<ScheduleWorkspace> scheduleWorkspaces = workspaceService.findById(id).getSchedule();
+        List<ScheduleWorkspaceDto> dtoList = new ArrayList<>();
+        for (ScheduleWorkspace scheduleWorkspace: scheduleWorkspaces){
+            dtoList.add(ScheduleWorkspaceMapper.toResponseDto(scheduleWorkspace));
+        }
+
+        return ResponseEntity.ok(dtoList);
     }
 }
--- src/main/java/kz/hackathon/meeting/dto/mappers/ScheduleWorkspaceMapper.java
+++ src/main/java/kz/hackathon/meeting/dto/mappers/ScheduleWorkspaceMapper.java
@@ -5,10 +5,14 @@ import kz.hackathon.meeting.models.ScheduleWorkspace;
 
 public class ScheduleWorkspaceMapper {
     public static ScheduleWorkspaceDto toResponseDto(ScheduleWorkspace scheduleWorkspace){
-        return ScheduleWorkspaceDto.builder()
+        ScheduleWorkspaceDto dto = ScheduleWorkspaceDto.builder()
                 .id(scheduleWorkspace.getId())
                 .date(String.valueOf(scheduleWorkspace.getDate()))
-                .account(AccountMapper.toResponseDto(scheduleWorkspace.getAccount()))
+//                .account(AccountMapper.toResponseDto(scheduleWorkspace.getAccount()))
                 .build();
+        if(scheduleWorkspace.getAccount()!=null){
+            dto.setAccount(AccountMapper.toResponseDto(scheduleWorkspace.getAccount()));
+        }
+        return dto;
     }
 }
